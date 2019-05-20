51c89b7d175f4023b8e059f74e056bf032f9b000
parwez <mdparwezalam920@gmail.com>
2019-05-20 16:13:46 +0530

    new sysout is added

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -9,6 +9,7 @@ public class Demo1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 		System.out.println("main method ");
+		System.out.println("Spring Demo Project");
 	}
 
 }
